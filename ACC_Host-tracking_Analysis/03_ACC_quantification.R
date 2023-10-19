b0354ef25e8ad20ccbf0059ed652dc91eb1caab4
ZoeHansen <47985088+ZoeHansen@users.noreply.github.com>
2023-10-19 12:05:38 -0500

    Update and rename ACC_quantification.R to 03_ACC_quantification.R

--- ACC_Host-tracking_Analysis/03_ACC_quantification.R
+++ ACC_Host-tracking_Analysis/03_ACC_quantification.R
@@ -1,10 +1,8 @@
 ########################################################
-
-# Aim 2: ACC Quantification Code
+# ACC Quantification Code
 
 # Author: Zoe Hansen
 # Last modified: 2022.01.12
-
 ########################################################
 
 # This code uses the "ACCs_per_sample" information retrieved from a Python script
@@ -21,10 +19,10 @@ library(ggfortify)
 library(vegan)
 library(calibrate)
 
-accs <- read.csv('D://Manning_ERIN/ERIN_FullDataset_AIM_TWO/ThirdAnalysis_MEGARes_v2/ACC_analysis/ERIN_total_ACCs_per_sample_CaseFollowPairs_wControls.csv',
+accs <- read.csv('D://ACC_analysis/ERIN_total_ACCs_per_sample_CaseFollowPairs_wControls.csv',
                  header = TRUE)
 
-meta = read.csv('D://Manning_ERIN/ERIN_FullDataset_AIM_TWO/ThirdAnalysis_MEGARes_v2/ERIN_Metagenomes_Metadata_60_CaseFollowPairs_wControls.csv', 
+meta = read.csv('D://ERIN_Metagenomes_Metadata_60_CaseFollowPairs_wControls.csv', 
                  header = TRUE)
 
 meta_sub <- meta %>%
@@ -32,6 +30,7 @@ meta_sub <- meta %>%
 
 acc_data <- left_join(meta_sub, accs, by=c('ER_ID', 'Case.status'))
 
+# Remove samples that did not align properly/contain ACCs; subset cases and follow-ups only
 acc_sub <- acc_data %>%
   filter(!grepl('ER0535', ER_ID))%>%
   filter(!grepl('ER0611', ER_ID))%>%
@@ -46,20 +45,14 @@ acc.minmax <- acc_sub %>%
   group_by(Case.status, Pathogen)%>%
   summarise(MinACC=min(num_accs), MaxACC=max(num_accs))
 
-health_comparisons <- list(c("Case", "Control"), c("FollowUp", "Control"), c("Case", "FollowUp"))
-colors.health = c('cyan4', 'darkorange3', 'darkorchid3')
-
 case.follow.comp <- list(c('Case','FollowUp'))
 colors.cf <- c('cyan4','darkorchid3')
 
-path.comp <- list(c('Campylobacter (CA)', 'Salmonella (SA)'))
-colors.path = c('dodgerblue2','firebrick2','goldenrod2','mediumpurple2')
-
-ggplot(data=acc_sub, aes(x=Pathogen, y=num_accs, color=Pathogen))+
+ggplot(data=acc_sub, aes(x=Case.status, y=num_accs, color=Case.status))+
   geom_boxplot() + 
-  geom_jitter(aes(shape=Pathogen, color=Pathogen))+
-  scale_color_manual(values=colors.path) +
-  scale_fill_manual(values=colors.path)+
+  geom_jitter(aes(shape=Case.status, color=Case.status))+
+  scale_color_manual(values=colors.cf) +
+  scale_fill_manual(values=colors.cf)+
   facet_wrap(~Case.status, scales="free") +
   theme_bw(base_size = 10) +
   theme(legend.position = 'none', 
@@ -72,7 +65,7 @@ ggplot(data=acc_sub, aes(x=Pathogen, y=num_accs, color=Pathogen))+
         strip.text.x = element_text(size = 11),
         panel.spacing.x = unit(1, 'lines'))+
   labs(
-    x = '\nPathogen',
+    x = '\nHealth Status',
     y = 'Number of ACCs\n'
   )+
 #  stat_pvalue_manual(means.to.plot,   label = "p.adj", tip.length = 0.01)
@@ -80,28 +73,13 @@ ggplot(data=acc_sub, aes(x=Pathogen, y=num_accs, color=Pathogen))+
                      label = 'p.format')
 
 
-# Get the stats (Case, Control, Follow)
-compare_means(num_accs ~ Case.status, acc_sub, method = 'wilcox.test')
-
-# Case-Follow Pairs
+# Wilcoxon Test - Case-Follow Pairs
 wilcox.test(num_accs ~ Case.status, data = acc_sub, paired = TRUE)
 
 means.to.plot <- tibble::tribble(
   ~group1,    ~group2,         ~p.adj,  ~y.position, ~variable,
   "Case",    "FollowUp",    2.744e-07,     850,   "num_accs"
 )
-
-# Pathogen
-
-acc_sub_case <- acc_sub %>%
-  filter(Case.status == 'Case')
-acc_sub_follow <- acc_sub %>%
-  filter(Case.status == 'FollowUp')
-
-compare_means(num_accs ~ Pathogen, data=acc_sub_case, method='wilcox.test')
-compare_means(num_accs ~ Pathogen, data=acc_sub_follow, method='wilcox.test')
-
-
 
 
 
